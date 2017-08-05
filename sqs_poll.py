d68d837a4d42a04965b811dd9b9f66d8c7e9314d
aayushberi26 <aayush.beri26@gmail.com>
2017-08-05 16:30:24 +0000

    added region to poll, tested

--- sqs_poll.py
+++ sqs_poll.py
@@ -1,24 +1,29 @@
+import time
+
 import boto3
 
 from request_modules import display_data
 from environment import (
     aws_access_key_id,
     aws_secret_access_key,
-    aws_queue_url
+    aws_queue_url,
+    aws_region_name
 )
 
-sqs = boto3.client('sqs',aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+sqs = boto3.client('sqs',aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, region_name=aws_region_name)
 
 def poll_and_run_script():
     while True:
         data = sqs.receive_message(
                     QueueUrl=aws_queue_url,
-                    MaxNumberOfMessages=10,
+                    MaxNumberOfMessages=1,
                     WaitTimeSeconds=20
                 )
         if 'Messages' in data:
             display_data()
+            time.sleep(60)
             sqs.purge_queue(QueueUrl=aws_queue_url)            
 
-
+if __name__ == '__main__':
+    poll_and_run_script()
 
